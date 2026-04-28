ee9124c7d7b2ec17c960f58fc5c31e47192c0d85
Glen Cao <glen.cao@amd.com>
2026-04-27 19:05:49 -0700

    Removed the decorator `@skipCUDAIfRocm(msg="launch bounds error out on ROCM")` from `test_nn::test_upsampling_64bit_indexing_channels_last` rather than only commentting it out.

--- test/test_nn.py
+++ test/test_nn.py
@@ -11003,7 +11003,6 @@ class TestNNDeviceType(NNTestCase):
             gradgradcheck(lambda x: F.interpolate(x, out_size, **kwargs), [input])
 
     @onlyCUDA
-    # @skipCUDAIfRocm(msg="launch bounds error out on ROCM")
     @dtypes(torch.half, torch.bfloat16)
     @largeTensorTest('40GB')
     def test_upsampling_64bit_indexing_channels_last(self, device, dtype):
