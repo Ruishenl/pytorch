378f4542c8faabf1465949c69673b1c2b7baabeb
Bob Ren <bobrenjc93@users.noreply.github.com>
2026-04-30 14:41:10 +0000

    Fix py3.14 reconstruction failure expectation

--- test/dynamo/test_error_messages.py
+++ test/dynamo/test_error_messages.py
@@ -137,7 +137,9 @@ def _load_global_has_positions() -> bool:
 
 
 def _reconstruction_failure_gb_stack_source_attribution() -> str:
-    if IS_MACOS or (sys.version_info >= (3, 11) and _load_global_has_positions()):
+    if sys.version_info < (3, 14) and (
+        IS_MACOS or (sys.version_info >= (3, 11) and _load_global_has_positions())
+    ):
         var_repr = "NullVariable originated from:"
     else:
         var_repr = (
